2054ced0587518d1dd7f4dc5d3b0d5062bf6a009
Johnathan Booy <johnny_music@icloud.com>
2022-09-13 02:07:58 -0700

    Step Six: Protecting Questions

--- app.py
+++ app.py
@@ -8,34 +8,45 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 # Empty list to be populated with users responses to the survey
-responses = [None for question in survey.questions]
+responses = []
 
 @app.route('/')
 def show_survey_start():
     '''Renders the homepage'''
-    responses = [None for question in survey.questions]
     title = survey.title
     instructions = survey.instructions
     return render_template('survey_start.html', title=title, instructions=instructions)
 
+@app.route('/begin')
+def start_survey():
+    '''Clear the responses'''
+    responses.clear()
+    return redirect('/questions/0')
+
+
 @app.route('/questions/<int:index>')
 def show_question(index):
     '''Renders the questions page for given index. User is prompted submit response.'''
-    question = survey.questions[index].question
-    choices = survey.questions[index].choices
-    return render_template('questions.html', index=index, question=question, choices=choices)
+    if len(responses) == len(survey.questions):
+        return redirect('/thanks')
+    elif index >= len(survey.questions) or index > len(responses):
+        return redirect(f'/questions/{len(responses)}')
+    else:
+        question = survey.questions[index].question
+        choices = survey.questions[index].choices
+        return render_template('questions.html', index=index, question=question, choices=choices)
 
-@app.route('/answer/<int:index>', methods=["POST"])
-def handle_answer(index):
+@app.route('/answer', methods=["POST"])
+def handle_answer():
     '''Stores the user's answer and redirects them to the next question.'''
     choice = request.form['choice']
-    responses[index] = choice
-    if None in responses:
-        next_index = responses.index(None)
-        return redirect(f'/questions/{next_index}')
+    responses.append(choice)
+    if len(responses) < len(survey.questions):
+        return redirect(f'/questions/{len(responses)}')
     else:
         return redirect('/thanks')
 
 @app.route('/thanks')
 def show_thanks():
-    return render_template('thanks.html')
+    results = zip([question.question for question in survey.questions], responses)
+    return render_template('thanks.html', results=results)
